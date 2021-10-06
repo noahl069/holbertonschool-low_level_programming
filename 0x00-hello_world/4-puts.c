2bfc32855659ab23937813ce6e352a295f5de149
NoahL <noah.leudiere78@gmail.com>
2021-10-06 11:32:46 +0200

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
+// C program to show the use of puts
 #include <stdio.h>
 int main()
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle.");
 
 	getchar();
 	return 0;
