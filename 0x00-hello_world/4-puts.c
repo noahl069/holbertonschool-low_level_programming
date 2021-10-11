57420837d1610ced9fb89934794a89bd84decbd6
NoahL <noah.leudiere78@gmail.com>
2021-10-11 10:12:45 +0200

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main(void)
-/* The function main is mandatory in all programs.*/
+/**function MAIN is mandatory in all programs.*/
 {
-	puts("Programming is like building a multilingual puzzle.");
+	puts(""Programming is like building a multilingual puzzle");
 
 	getchar();
 	return (0);
