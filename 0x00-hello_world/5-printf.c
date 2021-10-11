52d056b017ad2fda3be98d041413209dabfd6af6
NoahL <noah.leudiere78@gmail.com>
2021-10-11 11:24:07 +0200

    comit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	fputs("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,");
 	getchar();
 	return (0);
 }
