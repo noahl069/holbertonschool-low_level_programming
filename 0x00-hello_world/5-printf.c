c009e09f8853d76ab1f77d3b1a2a35dbbf64454a
NoahL <noah.leudiere78@gmail.com>
2021-10-11 14:37:43 +0200

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,8 @@
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, ");
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	getchar();
 	return (0);
 }
