4b565a9674bbfa1a15dc99873694957183e73c70
NoahL <noah.leudiere78@gmail.com>
2021-10-11 11:14:16 +0200

    commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - mandatory in all programs
+ * Return: return
+ */
+int main(void)
+{
+	fputs("with proper grammar, but the outcome is a piece of art,");
+	getchar();
+	return (0);
+}
